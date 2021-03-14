da1fdee78e6e53914526113066f56a1471a1c51f
YgalIdan <ygal.idan@gmail.com>
2021-03-14 22:30:15 +0200

    https://github.com/YgalIdan/123.git

---
+++ main.py
@@ -0,0 +1,22 @@
+# imports
+import random
+
+Colors = ["Blue", "Red", "Yellow", "Green"]
+pick = random.choice(Colors)
+print(pick)
+print("Please guess the color out of this list: " + str(Colors))
+user_pick = input()
+count = 1
+user = False
+
+while count < 3:
+    if user_pick.upper() == pick.upper():
+        count = count + 1
+        user = True
+        print("Success")
+        break
+    else:
+        user_pick = input()
+        count = count + 1
+        user = False
+        print("Error")
